7c93248dbe034fa865f170d80b343093adc89792
sadikaTabassum <ssadika.tabassum@gmail.com>
2025-12-16 13:26:01 +0600

    feat: add theme and UI providers

--- components/shared/FormSelector.tsx
+++ components/shared/FormSelector.tsx
@@ -1,35 +1,46 @@
-import { } from 'react';
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
-import { Select } from '@radix-ui/react-select';
-import { SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
+import {} from "react";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Select } from "@radix-ui/react-select";
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function FormSelector({ form }: { form: any }) {
-    return (
-        <FormField
-            control={form.control}
-            name="companySize"
-            render={({ field }) => (
-                <FormItem>
-                    <FormLabel>Company Size</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                        <FormControl>
-                            <SelectTrigger>
-                                <SelectValue placeholder="Select company size" />
-                            </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                            <SelectItem value="1-10">1-10 employees</SelectItem>
-                            <SelectItem value="11-50">11-50 employees</SelectItem>
-                            <SelectItem value="51-200">51-200 employees</SelectItem>
-                            <SelectItem value="201-500">201-500 employees</SelectItem>
-                            <SelectItem value="501-1000">501-1000 employees</SelectItem>
-                            <SelectItem value="1001+">1001+ employees</SelectItem>
-                        </SelectContent>
-                    </Select>
-                    <FormMessage />
-                </FormItem>
-            )}
-        />
-    );
-};
+  return (
+    <FormField
+      control={form.control}
+      name="companySize"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Company Size</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select company size" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem value="1-10">1-10 employees</SelectItem>
+              <SelectItem value="11-50">11-50 employees</SelectItem>
+              <SelectItem value="51-200">51-200 employees</SelectItem>
+              <SelectItem value="201-500">201-500 employees</SelectItem>
+              <SelectItem value="501-1000">501-1000 employees</SelectItem>
+              <SelectItem value="1001+">1001+ employees</SelectItem>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
